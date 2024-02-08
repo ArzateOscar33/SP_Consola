fc20e3ce12b90ad374ad782f53bbbc614a635095
CesatSoftware2 <software2@cesat.mx>
2024-02-07 20:39:18 -0800

    first commit

---
+++ ComunicacionSP_Consola/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComunicacionSP_Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidacionCOM validacionCOM = new ValidacionCOM();
+
+            validacionCOM.ValidarPuertosComParalelo();
+
+            List<string> puertosValidados = validacionCOM.ObtenerPuertosComValidados();
+
+            Console.WriteLine("Puertos COM validados:");
+            foreach (var puertoValidado in puertosValidados)
+            {
+                Console.WriteLine(puertoValidado);
+            }
+        }
+    }
+}
